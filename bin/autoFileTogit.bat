d73aa506ee3a6b1d67f62749874fe9048e110514
Misaki <1099249596@qq.com>
2024-06-06 16:27:31 +0800

    File

--- bin/autoFileTogit.bat
+++ bin/autoFileTogit.bat
@@ -2,4 +2,4 @@ git init
 git add -A
 git commit -m "File"
 
-git push -f https://github.com/Misakixyz/Misakixyz.github.io.git main
+git push -f https://github.com/Misakixyz/Misakixyz.github.io.git master:main
